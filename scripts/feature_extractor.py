2aa5879bdc6f2b1187545519e20cb88c1fb6995a
Lucio Giordano <45050786+GiordanoLucio@users.noreply.github.com>
2018-12-19 14:34:42 +0100

    Updated comments

--- scripts/feature_extractor.py
+++ scripts/feature_extractor.py
@@ -26,32 +26,31 @@ for folder in os.listdir(path):
             print('analyzing video: ', video)
             video_name = video[2:5]
             vidcap = cv2.VideoCapture(path + folder + '/' +video)
-			#load a frame in the variable 'frame' 
+		#load a frame in the variable 'frame' 
             success, frame = vidcap.read()
             #if the frame has been read succesfully
             if success:
-				#set number of elements analyzed = 0
+		#set number of elements analyzed = 0
                 elements_analyzed = 0
                 try:
                     succ = success
-					#start from 0.2s 
+		    #start from 0.2s 
                     vidcap.set(cv2.CAP_PROP_POS_MSEC, (2*100))
-						#encodes the first 10 good faces found in the video.
+	   	    #encodes the first 10 good faces found in the video.
                     while elements_analyzed < 10 and succ:
                         try:
-                            #data=[]
                             image = Image.fromarray(frame, 'RGB')
                             image = image.rotate(-90)
-							#uses hog feature but could also use 'cnn' as parameter to use Convolutional Neural Net to find the faces.
+			    #uses hog feature but could also use 'cnn' as parameter to use Convolutional Neural Net to find the faces.
                             boxes = face_recognition.face_locations(np.array(image), model="hog")
                             encode= face_recognition.face_encodings(np.array(image))
                             row = np.array(encode)
                             row = np.append(row, str(video_name))
                             if len(row) <3:
                                 print('could not find a face')
-								#the algorithm could find more then one face, so one would be a false positive.
-								#to avoid this, I discard every frame where it finds more faces.
-								#Remember: every face is encoded with one 128-d vector.
+			    #the algorithm could find more then one face, so one would be a false positive.
+			    #to avoid this, I discard every frame where it finds more faces.
+			    #Remember: every face is encoded with one 128-d vector.
                             elif len(row) == 129:
                                 csv_writer.writerow(row)
                                 elements_analyzed +=1
@@ -60,7 +59,7 @@ for folder in os.listdir(path):
                                 print('Frame discarded: found more than one face')                     
                         except Exception as e:
                             print('errore: ', e)
-							#reads the next frame
+			#reads next frame
                         succ,frame=vidcap.read()
                     succ = False      
                 except Exception as ex:
